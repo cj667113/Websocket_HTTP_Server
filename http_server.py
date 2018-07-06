81d9f3939afd7dc1ead3dce91ecc3e118ae14b00
Christopher M Johnston <40300884+cj667113@users.noreply.github.com>
2018-07-06 16:01:18 -0400

    Add files via upload

--- http_server.py
+++ http_server.py
@@ -3,17 +3,31 @@ import os
 from websocket_server import WebsocketServer
 import time
 import threading
+import ConfigParser
 import base64
+parser = ConfigParser.RawConfigParser()
+configFilePath =r'server.conf'
+parser.read(configFilePath)
+http_interface = parser.get('Server-Configurations', 'HTTP_Interface')
+http_port = parser.get('Server-Configurations', 'HTTP_Port')
+http_listen = parser.get('Server-Configurations', 'HTTP_Listen')
+http_listen = int(http_listen)
+http_recieve = parser.get('Server-Configurations', 'HTTP_Recieve')
+http_recieve = int(http_recieve)
+websocket_interface = parser.get('Server-Configurations', 'Websocket_Interface')
+websocket_port = parser.get('Server-Configurations', 'Websocket_Port')
+websocket_port = int(websocket_port)
+image_path = parser.get('Server-Configurations', 'Image')
 def http_server():
-	addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
+	addr = socket.getaddrinfo(http_interface, http_port)[0][-1]
 	s = socket.socket()
 	s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 	s.bind(addr)
-	s.listen(5)
+	s.listen(http_listen)
 	print('listening on', addr)
 	while True:
 		c1, addr = s.accept()
-        	print repr(c1.recv(4096))
+        	print repr(c1.recv(http_recieve))
         	print('client connected from',addr)
 		data = open("index.html", 'rb')
 		data = data.read()
@@ -22,38 +36,37 @@ def http_server():
        		c1.close()
 
 def websocket_server():
-	with open("thermal.jpg", "rb") as image_file:
-		thermal=base64.b64encode(image_file.read())
-	# Called for every client connecting (after handshake)
+	global i
+	i=0
 	def new_client(client, server):
+		global i
+		i=((i+1))
+		def start_data_stream():
+			while True:
+				try:
+					with open(image_path, "rb") as image_file:
+						thermal=base64.b64encode(image_file.read())
+					server.send_message_to_all(thermal)
+					time.sleep(.02)
+				except:
+					with open(image_path,"rb") as image_file:
+						thermal=base64.b64encode(image_file.read())
+					server.send_message_to_all(thermal)
+					time.sleep(.02)
+		if i == 1:
+			start_data_stream()
+		else:
+			pass
+
 	        print("New client connected and was given id %d" % client['id'])
-	#       server.send_message_to_all("Hey all, client(%d) has joined us" % client['id'])
-		while True:
-			try:
-				with open("thermal.jpg", "rb") as image_file:
-					thermal=base64.b64encode(image_file.read())
-				time.sleep(.015)
-				server.send_message_to_all(thermal)
-				time.sleep(.025)
-			except:
-				pass
-	# Called for every client disconnecting
+
 	def client_left(client, server):
 	        print("Client(%d) disconnected" % client['id'])
-		#server.send_message_to_all("Hey all, client(%d) had disconnected" % client['id'])
-
-	# Called when a client sends a message
-	def message_received(client, server, message):
-	        if len(message) > 200:
-	                message = message[:200]+'..'
-	        print("Client(%d) said: %s" % (client['id'], message))
 
 	PORT=9876
-	server = WebsocketServer(PORT,host="0.0.0.0")
+	server = WebsocketServer(websocket_port,host=websocket_interface)
 	server.set_fn_new_client(new_client)
 	server.set_fn_client_left(client_left)
-	server.set_fn_message_received(message_received)
 	server.run_forever()
-
 threading.Thread(target=websocket_server).start()
 threading.Thread(target=http_server).start()
